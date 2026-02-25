788494af4a8c49650008c78688952a2c0707c348
andsprague1 <andsprague1@gmail.com>
2026-02-25 17:41:26 -0500

    Add project files.

---
+++ Feburary 25th 2026/Feburary 25th 2026.cpp
@@ -0,0 +1,36 @@
+// Feburary 25th 2026.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	//
+	string weather = "rainy";
+
+	cin >> weather;
+
+	if (weather == "Sunny")
+	{
+		cout << "It's Bright out. Beach weather!\n";
+	}
+	else
+	{
+		cout << "It's not clear out.";
+	}
+	cout << "And that's the Weather";
+	
+
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
